6c60497fa06357051de86307a3c24f2829e14d49
Camilla Murgia <c.murgia4@studenti.uniss.it>
2020-11-14 20:36:43 +0100

    implemented backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+puts 'Creating ingredients...'
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+Ingredient.create(name: 'tomato souce')
+Ingredient.create(name: 'tabasco')
+Ingredient.create(name: 'tequila')
+Ingredient.create(name: 'vodka')
+Ingredient.create(name: 'tonic water')
+Ingredient.create(name: 'gin')
+puts 'Ingredients created'
+
+puts 'Creating cocktails...'
+Cocktail.create(name: 'BloodyMary')
+Cocktail.create(name: 'MoscowMule')
+puts 'Cocktails created'
